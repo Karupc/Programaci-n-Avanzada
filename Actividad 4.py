b3ae519ea43a9d57f8b73a1b4905fb45a8cc505a
Karup <kperca802@gmail.com>
2025-07-20 21:26:20 -0600

    Ingreso de datos

---
+++ Actividad 4.py
@@ -0,0 +1,7 @@
+# Simulador de votación con validación cruzada
+print("Por favor ingrese los siguientes datos:")
+nombre_completo = input("Nombre completo: ")
+dpi = input("Número de DPI: ")
+departamento = input("Departamento: ")
+anio_nacimiento = int(input("Año de nacimiento: "))
+edad = 2025 - anio_nacimiento
